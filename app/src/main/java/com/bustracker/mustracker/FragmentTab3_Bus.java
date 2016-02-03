1a4d17276941d5d7d742f4bb366494cf1cc2ce2b
kotcharatp <pear.kc@gmail.com>
2016-02-03 14:53:34 +0700

    Bus Schedule page update google map route

--- app/src/main/java/com/bustracker/mustracker/FragmentTab3_Bus.java
+++ app/src/main/java/com/bustracker/mustracker/FragmentTab3_Bus.java
@@ -3,6 +3,8 @@ package com.bustracker.mustracker;
 /**
  * Created by kotcharat on 1/31/16.
  */
+import android.graphics.Color;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,17 +12,99 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Spinner;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+
 public class FragmentTab3_Bus extends Fragment {
+
+    private static GoogleMap mMap;
+    private static Double latitude, longitude;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_bus, container, false);
 
         Spinner route = (Spinner)rootView.findViewById(R.id.spinner);
 
+        latitude = 13.792686;
+        longitude = 100.326425;
 
+        setUpMapIfNeeded();
 
         return rootView;
+    }
+
+    public void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.location_map)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null)
+                setUpMap();
+        }
+    }
+
+    // Server = AIzaSyBumsm3eWV31p1xqlt5yona3yeVaw9XjHc
+
+    private static void setUpMap() {
+        // For showing a move to my loction button
+        mMap.setMyLocationEnabled(true);
+        // For dropping a marker at a point on the Map
+        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Mahidol University").snippet("Main Station"));
+        // For zooming automatically to the Dropped PIN Location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,
+                longitude), 13.0f));
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.addPolyline(
+                new PolylineOptions().add(new LatLng(13.788112, 100.327534)).add(new LatLng(latitude, longitude)).color(Color.BLUE));
+        mMap.addPolyline(
+                new PolylineOptions().add(new LatLng(13.780407, 100.442659)).add(new LatLng(13.788112, 100.327534)).color(Color.BLUE));
+
+        String serverKey = "AIzaSyBumsm3eWV31p1xqlt5yona3yeVaw9XjHc";
+        LatLng origin = new LatLng(37.7849569, -122.4068855);
+        LatLng destination = new LatLng(37.7814432, -122.4460177);
+
+
+        /*
+        GoogleDirection.withServerKey(serverKey)
+                .from(origin)
+                .to(destination)
+                .execute(new DirectionCallback() {
+                    @Override
+                    public void onDirectionSuccess(Path.Direction direction) {
+                        // Do something here
+                    }
+
+                    @Override
+                    public void onDirectionFailure(Throwable t) {
+                        // Do something here
+                    }
+                });*/
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        if (mMap != null)
+            setUpMap();
+
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.location_map)).getMap(); // getMap is deprecated
+            // Check if we were successful in obtaining the map.
+            if (mMap != null)
+                setUpMap();
+        }
     }
 
 }
